2cafa0d3b295dc0451dcc78c7452b5b73578507f
Raul Marx <raul.marx.rm@gmail.com>
2022-10-26 17:54:38 -0300

    verifica se esta logado

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -1,16 +1,23 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { AuthProvider } from "./contexts/Auth/AuthContext";
+import { useContext } from "react";
+import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
+import { AuthContext, AuthProvider } from "./contexts/Auth/AuthContext";
 import { Home } from "./pages/home";
 import Login from "./pages/login";
 
 const AppRoutes = () => {
-
+    const Private = ({children}) =>{
+        const {authenticated} = useContext(AuthContext);
+        if(!authenticated){
+            <Navigate to="/login" />
+        }
+        return children;
+    }
     return (
         <Router>
             <AuthProvider>
                 <Routes>
                     <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Private><Home /></Private>} />
                 </Routes>
             </AuthProvider>
         </Router>
